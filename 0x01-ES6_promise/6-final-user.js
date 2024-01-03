2c26709a24a4bd389a59a4e3d20673a3d43bac28
elyse502 <elyseniyibizi502@gmail.com>
2024-01-03 09:43:37 +0000

    Return an array with the following structure

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
+}

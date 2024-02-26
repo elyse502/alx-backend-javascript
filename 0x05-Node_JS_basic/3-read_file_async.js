c62bf210c2439f8773fc96c33503a3f6b19ff588
elyse502 <elyseniyibizi502@gmail.com>
2024-02-26 06:10:14 +0000

    Add Solution to Task 3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const countStudents = async (path) => {
+  let data;
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  // Split where a new line exists
+  const students = data.split('\n')
+    // Turn a row into an array by splitting by a ','
+    .map((row) => row.split(','))
+    // Skip first row
+    .filter((row) => row.length === 4 && row[0] !== 'firstname')
+    // Convert into objects
+    .map((row) => ({
+      firstName: row[0],
+      lastName: row[1],
+      age: row[2],
+      field: row[3].replace('\r', ''),
+    }));
+  // Generate CS students
+  const CS = students.filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  // Generate SWE students
+  const SWE = students.filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  // Print length and convert each into a string
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+  return { students, CS, SWE };
+};
+
+module.exports = countStudents;
